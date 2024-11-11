978c0bcb832cf2e62c858c2e4884a033b1625471
EllaaManu <manucarmona0548@gmail.com>
2024-11-10 22:02:44 -0300

    Arrumando Excluir

--- Persistencia/produtoDAO.js
+++ Persistencia/produtoDAO.js
@@ -113,9 +113,9 @@ export default class ProdutoDAO {
     async excluir(produto) {
         if (produto instanceof Produto) {
             const conexao = await conectar();
-            const sql = `DELETE FROM produto WHERE codigo = ?`;
+            const sql = `DELETE FROM produto WHERE prod_codigo = ?`;
             let parametros = [
-                produto.codigo
+                produto.prod_codigo
             ]; //dados do produto
             await conexao.execute(sql, parametros);
             await conexao.release(); //libera a conexão
